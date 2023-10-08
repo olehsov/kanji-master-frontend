80aa8cde4cdbae9e196b11b66ecfbd6b7538d4b7
Oleh Skrypnyk <oskrypnyk@griddynamics.com>
2023-10-08 23:39:55 +0200

    Add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import {PageModule} from "./pages/page.module";
-import {SharedModule} from "primeng/api";
 import {CustomSharedModule} from "./shared/custom-shared.module";
-import {DefaultAppComponentsModule} from "./shared/default-app-components/default-app-components.module";
 
 @NgModule({
   declarations: [
--- src/app/shared/prime-ng-shared.module.ts
+++ src/app/shared/prime-ng-shared.module.ts
@@ -1,7 +1,4 @@
 import { NgModule } from '@angular/core';
-import {CommonModule, NgOptimizedImage} from '@angular/common';
-import { TopBarComponent } from './default-app-components/top-bar/top-bar.component';
-import {DefaultAppComponentsModule} from "./default-app-components/default-app-components.module";
 import {ButtonModule} from "primeng/button";
 
 
