eaf791787e96419ab7f59af8549054385e3104c5
Oleh Skrypnyk <oskrypnyk@griddynamics.com>
2025-01-15 06:43:05 +0100

    Update evs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,5 @@ import {IEnvironment} from "./environment.interface";
 
 export const environment: IEnvironment = {
     logoPath: '/assets/images/kanji-master-logo.png',
-    // apiUrl: 'https://kanji-master-backend-695666739311.europe-west12.run.app/graphql'
-    apiUrl: 'http://localhost:8080/graphql'
+    apiUrl: 'https://kanji-master-backend-695666739311.europe-west12.run.app/graphql'
 };
